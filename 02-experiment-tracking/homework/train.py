2c863c1e84b17cb2e8455271eb925e80e7ba8573
ujjwal4869raj <163645573+ujjwal4869raj@users.noreply.github.com>
2024-07-15 13:58:46 +0000

    new files

---
+++ 02-experiment-tracking/homework/train.py
@@ -0,0 +1,52 @@
+import os
+import pickle
+import click
+
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+
+
+def run_train(data_path: str):
+
+    mlflow.set_experiment("my-experiment-1")
+
+    with mlflow.start_run():
+
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+        params = {"max_depth": 10, "random_state": 0}
+        mlflow.log_params(params)
+
+        rf = RandomForestRegressor(**params)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        #mlflow.log_metric("accuracy", accuracy_score(y_val, y_pred))
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        print(rmse)
+        
+        mlflow.sklearn.log_model(rf, artifact_path="models")
+        print(f"default artifacts URI: '{mlflow.get_artifact_uri()}'")
+
+
+if __name__ == '__main__':
+    run_train()
